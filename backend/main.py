a24486f71d476246568a77b289fc3c1821259f00
DarjaElina <daria.elina98@gmail.com>
2026-02-07 12:26:09 +0200

    Add create, read-by-id, and delete transaction endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
 from sqlmodel import select
 
 from db import create_db_and_tables, engine, SessionDep
@@ -23,3 +23,26 @@ def read_transactions(
 ) -> list[Transaction]:
   transactions = session.exec(select(Transaction).offset(offset).limit(limit)).all()
   return transactions
+
+@app.get("/transactions/{transaction_id}")
+def read_transaction(transaction_id: int, session: SessionDep) -> Transaction:
+  transaction = session.get(Transaction, transaction_id)
+  if not transaction:
+    raise HTTPException(status_code=404, detail="Transaction not found")
+  return transaction
+
+@app.post("/transactions/")
+def create_transaction(transaction: Transaction, session: SessionDep) -> Transaction:
+  session.add(transaction)
+  session.commit()
+  session.refresh(transaction)
+  return transaction
+
+@app.delete("/transactions/{transaction_id}")
+def delete_transaction(transaction_id: int, session: SessionDep):
+  transaction = session.get(Transaction, transaction_id)
+  if not transaction:
+    raise HTTPException(status_code=404, detail="Transaction not found")
+  session.delete(transaction)
+  session.commit()
+  return {"ok": True}
