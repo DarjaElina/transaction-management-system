c83c017206e2996efdea8183c1e93a9607d2b4cd
DarjaElina <daria.elina98@gmail.com>
2026-02-07 12:34:57 +0200

    Add SQL indexes to Transaction's date and category fields

--- backend/models/transaction.py
+++ backend/models/transaction.py
@@ -2,7 +2,7 @@ from sqlmodel import Field, SQLModel
 
 class Transaction(SQLModel, table=True):
   id: int | None = Field(default=None, primary_key=True)
-  date: str
+  date: str = Field(index=True)
   description: str
-  category: str
+  category: str = Field(index = True)
   amount: float
