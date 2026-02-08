4e67ce20218cd08ca5e4740bdb36ee4aebb20929
DarjaElina <daria.elina98@gmail.com>
2026-02-08 20:03:33 +0200

    Test workflow

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,3 +19,8 @@ app.include_router(transactions.router)
 @app.get("/")
 async def root():
     return {"message": "Hello! :-)"}
+
+
+@app.get("/myfrogs")
+async def root():
+    return {"Frogs": "🐸🐸🐸🐸🐸"}
