860ecbbbb3e206e63bc0ffd92ead57d20ae8580f
DarjaElina <daria.elina98@gmail.com>
2026-02-09 12:34:56 +0200

    Make amount field Decimal type

--- backend/app/models/transaction.py
+++ backend/app/models/transaction.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from sqlmodel import Field, SQLModel
 from datetime import datetime
 
@@ -6,7 +8,7 @@ class TransactionBase(SQLModel):
     date: datetime = Field(index=True)
     description: str
     category: str = Field(index=True)
-    amount: float
+    amount: Decimal = Field(default=0, max_digits=19, decimal_places=2)
 
     __tablename__ = "transactions"
 
@@ -29,4 +31,4 @@ class TransactionUpdate(TransactionBase):
     date: datetime | None = None
     description: str | None = None
     category: str | None = None
-    amount: float | None = None
+    amount: Decimal | None = None
