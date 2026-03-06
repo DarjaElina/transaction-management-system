f04e4e5b45d5edef4e25761e3171e77349d04bf3
DarjaElina <daria.elina98@gmail.com>
2026-03-06 23:10:32 +0200

    Add DataTablePagination reusable component

--- frontend/src/components/TransactionsTable/TransactionsTable.tsx
+++ frontend/src/components/TransactionsTable/TransactionsTable.tsx
@@ -32,6 +32,7 @@ import {
 } from '@/components/ui/dropdown-menu'
 
 import { useState } from 'react'
+import { DataTablePagination } from './DataTablePagination'
 
 interface TransactionsTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -157,29 +158,8 @@ export function TransactionsTable<TData, TValue>({
         </Table>
       </div>
 
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{' '}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
-        <div>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
+      <div className="my-4">
+        <DataTablePagination table={table} />
       </div>
     </div>
   )
