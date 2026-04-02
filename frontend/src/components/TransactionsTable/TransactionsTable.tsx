7288a6786bc3068c532bf52c04c1debe7216c73a
DarjaElina <daria.elina98@gmail.com>
2026-04-02 17:50:54 +0300

    Remove unused imports

--- frontend/src/components/TransactionsTable/TransactionsTable.tsx
+++ frontend/src/components/TransactionsTable/TransactionsTable.tsx
@@ -22,17 +22,11 @@ import {
   TableRow,
 } from '@/components/ui/table'
 
-import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 
 import { useState } from 'react'
 import { DataTablePagination } from './DataTablePagination'
+import { DataTableViewOptions } from './DataTabeleViewOptions'
 
 interface TransactionsTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -80,32 +74,7 @@ export function TransactionsTable<TData, TValue>({
           }
           className="max-w-sm"
         />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
-              Columns
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {table
-              .getAllColumns()
-              .filter((column) => column.getCanHide())
-              .map((column) => {
-                return (
-                  <DropdownMenuCheckboxItem
-                    key={column.id}
-                    className="capitalize"
-                    checked={column.getIsVisible()}
-                    onCheckedChange={(value) =>
-                      column.toggleVisibility(!!value)
-                    }
-                  >
-                    {column.id}
-                  </DropdownMenuCheckboxItem>
-                )
-              })}
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <DataTableViewOptions table={table} />
       </div>
       <div className="overflow-hidden rounded-md border">
         <Table>
