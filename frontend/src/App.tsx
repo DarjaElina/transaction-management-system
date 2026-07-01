98583296c4bcdf8416f889c531bfeb6a442bcb2d
Darja Elina <116391967+DarjaElina@users.noreply.github.com>
2026-07-01 15:01:53 +0300

    Add routing and create initial Dashboard page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
-import { TransactionsTable } from './components/TransactionsTable/TransactionsTable'
-import { columns } from './components/TransactionsTable/Columns'
 import { getTransactons } from './api/transactions'
-import { Toaster } from 'sonner'
-import { TransactionRowSkeleton } from './components/TransactionRowSkeleton'
-import { ThemeProvider } from './components/ThemeProvider'
-import { ModeToggle } from './components/ModeToggle'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import { mapTransactions } from './helpers'
+import Transactions from './pages/Transactions'
+import Dashboard from './pages/Dashboard'
+import Layout from './pages/Layout'
+import { ThemeProvider } from './components/ThemeProvider'
 
 function App() {
   const result = useQuery({
@@ -15,44 +14,27 @@ function App() {
   })
 
   const transactions = result.data ?? []
-
   const mappedTransactions = mapTransactions(transactions)
-
-  console.log(transactions)
 
   return (
     <ThemeProvider>
-      <div className="min-h-screen bg-muted/40">
-        <div className="mx-auto max-w-6xl px-6 py-10">
-          <nav className="w-full flex justify-end">
-            <ModeToggle />
-          </nav>
-
-          <div className="mb-8 flex">
-            <h1 className="text-3xl font-semibold">Transactions</h1>
-          </div>
-
-          <div className="bg-background rounded-xl border p-6">
-            {result.error && (
-              <p className="text-rose-500">Something went wrong 😿</p>
-            )}
-
-            {result.isLoading && (
-              <div className="space-y-2">
-                {Array.from({ length: 5 }).map((_, i) => (
-                  <TransactionRowSkeleton key={i} />
-                ))}
-              </div>
-            )}
-
-            {!result.isLoading && !result.error && (
-              <TransactionsTable columns={columns} data={mappedTransactions} />
-            )}
-          </div>
-
-          <Toaster richColors />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route
+              index
+              element={
+                <Transactions
+                  transactions={mappedTransactions}
+                  loading={result.isLoading}
+                  error={result.error}
+                />
+              }
+            />
+            <Route path="dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
